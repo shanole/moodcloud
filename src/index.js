f8a9d6dd4a74199b83e2a426ee86e13863b7ea9a
shanole <73411129+shanole@users.noreply.github.com>
2021-09-21 16:47:16 -0700

    add firebase

--- src/index.js
+++ src/index.js
@@ -4,17 +4,31 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { dashboardReducer } from './reducers/dashboard-reducer';
+import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
-const store = createStore(dashboardReducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() =>
   console.log(store.getState())
 );
 
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 ReactDOM.render(
   <Provider store = {store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+     <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
