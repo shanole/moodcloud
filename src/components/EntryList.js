367dff558a98bf04efed8ce49e1a7f92ace0b1af
shanole <73411129+shanole@users.noreply.github.com>
2021-09-26 17:14:55 -0700

    very simple pagination

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Entry from './Entry';
 import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
-
+import { getSnapshotByObject } from 'redux-firestore';
 
 function EntryList(props) {
-  // order by date
-  useFirestoreConnect({collection: 'entries', orderBy: ['timestamp', 'desc']});
-  
-  const entries = useSelector(state => state.firestore.ordered.entries);
+  const [cursor, setCursor] = useState(null);
 
+  const updateCursor = () => {
+    setCursor(getSnapshotByObject(entries[entries.length-1]));
+  }
+  // order by date
+  useFirestoreConnect({collection: 'entries', orderBy: ['timestamp', 'desc'], limit: 3, ...(cursor && { startAfter: cursor})});
+
+  const entries = useSelector(state => state.firestore.ordered.entries);
+  
   if (isLoaded(entries) && !isEmpty(entries)) {
     return(
       <React.Fragment>
+        <button onClick={() => setCursor(null)}>Back to Top</button>
+      <div style={{height: "300px", overflowY: "auto"}}>
         {entries.map( entry => {
           return <Entry key={entry.id} entryContent={entry}/>
         })}
+        <button onClick={updateCursor}>more</button>
+      </div>
       </React.Fragment>
     )
   } else if (isLoaded(entries) && isEmpty(entries)) {
