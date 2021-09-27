374be357b02beafaf9c6b2971b9577ef0249f587
shanole <73411129+shanole@users.noreply.github.com>
2021-09-27 10:03:43 -0700

    simple pagination works with live reloading

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Entry from './Entry';
 import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
-
+import { getSnapshotByObject } from 'redux-firestore';
 
 function EntryList(props) {
+  const [pointer, setPointer] = useState(null);
+
+  const updatePointer = () => {
+    if (isLoaded(entries)) {
+      setPointer(getSnapshotByObject(entries[entries.length-1]))
+    }
+  }
   // order by date
-  useFirestoreConnect({collection: 'entries', orderBy: ['timestamp', 'desc']});
+  useFirestoreConnect({collection: 'entries', orderBy: ['timestamp', 'desc'], limit: 2, ...(pointer && { startAfter: pointer })});
   
   const entries = useSelector(state => state.firestore.ordered.entries);
 
   if (isLoaded(entries) && !isEmpty(entries)) {
     return(
       <React.Fragment>
+        <button onClick={() => setPointer(null)}>Back to Top</button>
         {entries.map( entry => {
           return <Entry key={entry.id} entryContent={entry}/>
         })}
+        <button onClick={updatePointer}>See More</button>
       </React.Fragment>
     )
   } else if (isLoaded(entries) && isEmpty(entries)) {
     return(
-      <h3>No entries yet.</h3>
+      <React.Fragment>
+        <button onClick={() => setPointer(null)}>Back to Top</button>
+        <h3>No entries.</h3>
+      </React.Fragment>
     )
   } else {
     return(
