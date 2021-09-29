11fb62e136e60bccd5ecfadf6e7e95d4a7114332
shanole <73411129+shanole@users.noreply.github.com>
2021-09-28 22:41:41 -0700

    add bg image assets

--- src/components/auth/styles/StyledAccountControl.js
+++ src/components/auth/styles/StyledAccountControl.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
+import gradientbg from './../../../assets/img/gradient2.jpg'
 
 export default styled.div`
   height: 100vh;
+  position: relative;
+  background-color: #E1A7B0;
+  background: url(${gradientbg}) no-repeat center center/cover;
+  position: relative;
+  color: white;
+
   .content {
     display: flex;
     flex-direction: column;
