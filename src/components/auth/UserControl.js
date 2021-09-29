7970285c93461077fcdd530485b0333a5027f56f
shanole <73411129+shanole@users.noreply.github.com>
2021-09-29 10:39:33 -0700

    can change display name and profile pic

--- src/components/auth/UserControl.js
+++ src/components/auth/UserControl.js
@@ -1,27 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux'
-import { useFirebase, isLoaded } from 'react-redux-firebase'
+import { useFirebase, useFirebaseConnect, isLoaded } from 'react-redux-firebase'
 
 
 function UserControl() {
-  const firebase = useFirebase()
-  const profile = useSelector(state => state.firebase.profile)
+  const [formVisible, setFormVisible] = useState(false);
+  const [file, setFile] = useState(null)
+  const [uploadProgress, setUploadProgress] = useState(0);
 
-  function updateUserProfile() {
-    console.log('update clicked');
-    console.log(profile)
-    return firebase.updateProfile({ displayName: 'shannon' })
+  const firebase = useFirebase();
+  useFirebaseConnect('profile')
+  const profile = useSelector(state => state.firebase.profile);
+  const storage = firebase.storage();
+
+  const onFileChange = (e) => {
+    setFile(e.target.files[0])
   }
 
+  const onUpload = (event) => {
+    event.preventDefault();
+    const storageRef = storage.ref();
+
+    if (file) {
+      const fileRef = storageRef.child(file.name)
+      const uploadTask = fileRef.put(file);
+  
+      uploadTask.on('state_changed', 
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+        console.log('Upload is ' + progress + '% done');
+        
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, 
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error.message);
+      }, 
+      () => {
+        setFile(null);
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadUrl) => {
+          console.log('File available at', downloadUrl);
+          setFormVisible(false);
+          return firebase.updateProfile({ displayName: event.target.displayName.value, photoUrl: downloadUrl })
+        });
+      }
+    );
+    }
+  setFormVisible(false);
+  return firebase.updateProfile({displayName: event.target.displayName.value})
+  }
+
+  let visibleComponent;
+
+  if (formVisible) {
+    visibleComponent = (
+    <div>
+      <form onSubmit={onUpload}>
+        <label htmlFor='displayName'>display name:</label>
+        <input type='text' name='displayName' defaultValue={profile.displayName} />
+        <label htmlFor='photoUrl'>upload profile picture:</label>
+        <input type="file" onChange={onFileChange}/> 
+        {(file && uploadProgress > 0) && (
+            <div className="progress">
+              <div
+                className="progress-bar progress-bar-info progress-bar-striped"
+                role="progressbar"
+                aria-valuenow={uploadProgress}
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{ width: uploadProgress + "%", backgroundColor: 'red' }}
+              >
+                {uploadProgress}%
+              </div>
+            </div>
+          )}
+        <button type='submit'>save</button>
+      </form>
+
+      <button onClick={() => setFormVisible(false)}>
+        back
+      </button>
+      
+    </div>)
+  } else { 
+    visibleComponent = (<div>
+    <p>display name: {profile.displayName}</p>
+    <p>email: {profile.email}</p> 
+    <button onClick={() => setFormVisible(true)}>edit profile</button>
+  </div>
+  )
+  }
   return (
     <div>
-      <h2>Update User Profile</h2>
-      <span>
-        Click the button to update profile to include role parameter
-      </span>
-      <button onClick={updateUserProfile}>
-        Add Role To User
-      </button>
+      <h2>user profile</h2>
+      <img src={profile.photoUrl} width="300" height="300" alt="profile-pic"  style={{borderRadius: '50%'}}/>
+      <h3>hello, {profile.displayName}</h3>
+      {visibleComponent}
       <div>
         {
           isLoaded(profile)
--- src/components/dashboard/UserDetails.js
+++ src/components/dashboard/UserDetails.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
+import { isLoaded } from 'react-redux-firebase'
 import { showDashboard, showForm } from './../../actions/index';
 import { Link } from 'react-router-dom';
 
 function UserDetails() {
-
-  const [formVisible, setFormVisible] = useState(false);
-  const [profilePic, setProfilePic] = useState('https://firebasestorage.googleapis.com/v0/b/moodcloud-b54e6.appspot.com/o/cloud%20icon.jpg?alt=media&token=67e6638c-40b2-4f3b-ae29-411318fd6bb3')
+  // maybe move default image to register portion?
+  // const [profilePic, setProfilePic] = useState('https://firebasestorage.googleapis.com/v0/b/moodcloud-b54e6.appspot.com/o/cloud%20icon.jpg?alt=media&token=67e6638c-40b2-4f3b-ae29-411318fd6bb3')
 
   const profile = useSelector(state => state.firebase.profile);
 
   const dispatch = useDispatch();
   
-  if (profile.photoUrl !== undefined ) {
-    setProfilePic(profile.photoUrl);
-  }
-
   return (
     <div>
-      <img src={profilePic} width="300" alt="profile-pic"  style={{borderRadius: '50%'}}/>
+      <img src={profile.photoUrl} width="300" height="300" alt="profile-pic"  style={{borderRadius: '50%'}}/>
       <h3>Hello, {profile.displayName}</h3>
       <p><Link to='/account'>Edit user details</Link></p>
       <p onClick={() => dispatch(showForm('new'))}>New post</p>
